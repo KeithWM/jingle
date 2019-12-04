a05f493d3cb9d00d317c5fe6a30ead685e6ed7c4
KeithWM <keith@myerscough.nl>
2019-12-04 14:14:57 +0100

    till having difficulty with the tming, saving this to make a new branch

--- lib/pygame_output.py
+++ lib/pygame_output.py
@@ -1,29 +1,49 @@
-import pygame
+import pygame as pg
 
 from output import Output
+
+
+WHITE = (255, 255, 255)
+GREY = (128, 128, 128)
+
+
+class Tile(pg.sprite.Sprite):
+    def __init__(self, position):
+        super().__init__()
+        self.image = pg.Surface((100, 100))
+        self.color = WHITE
+        self.image.fill(self.color)
+        self.ref = self.image.get_rect(topleft=position)
+
+    def change_color(self, color):
+        self.color = color
+        self.image.fill(self.color)
 
 
 class PygameOutput(Output):
     def __init__(self, chunk, rate, width=300, height=300):
         super().__init__(chunk, rate)
-        pygame.init()
+        pg.init()
         self.fps = rate // chunk
-        self.clock = pygame.time.Clock()
-        self.colors = [(255, 255, 255), (128, 128, 128)]
-        self.screen = pygame.display.set_mode((width, height))
-        self.background = pygame.Surface((width, height))
+        self.clock = pg.time.Clock()
+        self.colors = [WHITE, GREY]
+        self.screen = pg.display.set_mode((width, height))
+        self.background = pg.Surface((width, height))
+        self.tile = Tile((100, 100))
         self.is_on = False
 
     def on(self):
-        pygame.draw.rect(self.screen, self.colors[0], pygame.Rect(100, 100, 100, 100))
+        self.screen.fill(WHITE)
+        # self.tile.change_color(WHITE)
 
     def off(self):
-        pygame.draw.rect(self.screen, self.colors[1], pygame.Rect(100, 100, 100, 100))
+        self.screen.fill(GREY)
+        # self.tile.change_color(GREY)
 
     def callback(self):
-        pygame.display.flip()
+        pg.display.flip()
         # self.clock.tick(self.fps)
-        for _ in pygame.event.get():
+        for event in pg.event.get():
             pass
 
 
@@ -35,7 +55,9 @@ if __name__ == "__main__":
     n_keep = 1000
     keep = np.zeros(n_keep)
 
-    outp = PygameOutput(2**10, 44800)
+    chunk = 2 ** 10
+    rate = 44800
+    outp = PygameOutput(chunk, rate)
 
     now = time.time()
     for i in range(n_keep):
@@ -47,5 +69,5 @@ if __name__ == "__main__":
         now, prev = time.time(), now
         keep[i] = now - prev
 
-    plt.plot(keep)
+    plt.plot(keep * rate / chunk)
 
--- main.py
+++ main.py
@@ -12,38 +12,49 @@ if __name__ == "__main__":
     RATE = 48000
 
     outp = output.factory('pygame', CHUNK, RATE)
-    inp = input.factory('audio', CHUNK, RATE)
+    outp.callback()
+
     ctrl = controller.Controller()
 
     n_keep = 1000
     i_keep = 0
-    keep = np.zeros((n_keep, 5))
+    keep = np.zeros((n_keep, 7))
 
-    prev = time.time()
-
+    inp = input.factory('audio', CHUNK, RATE)
     value_gen = inp.generate_values()
     # for value in value_gen:
-    while True:
-        print(i_keep, time.time() - prev)
-        value = next(value_gen)
-        prev = time.time()
-        outp.callback()
-        decision = ctrl.decide(value)
-        if decision:
-            outp.on()
-        else:
-            outp.off()
-        keep[i_keep, 0] = value
-        keep[i_keep, 1] = ctrl.rolling_avg.value * ctrl.THRESHOLD
-        keep[i_keep, 2] = decision
-        keep[i_keep, 3] = ctrl.mode == 'listening'
-        keep[i_keep, 4] = -int(ctrl.mode == 'playing')
-        i_keep = i_keep + 1
-        if i_keep == len(keep):
-            break
+    try:
+        while True:
+            prev = time.time()
+            value = next(value_gen)
+            delta_time = time.time() - prev
 
-    fig, (ax, bin_ax) = plt.subplots(2, 1, sharex=True)
-    # bin_ax = ax.twinx()
-    ax.plot(keep[:, :2])
-    bin_ax.plot(keep[:, 2:])
-    plt.show()
+            keep[i_keep, 2] = delta_time * RATE / CHUNK
+            print(f'{i_keep}, A: {delta_time}')
+
+            prev = time.time()
+            outp.callback()
+            delta_time = time.time() - prev
+
+            print(f'{i_keep}, B: {delta_time}')
+            keep[i_keep, 3] = delta_time * RATE / CHUNK
+
+            decision = ctrl.decide(value)
+            if decision:
+                outp.on()
+            else:
+                outp.off()
+            keep[i_keep, 0] = value
+            keep[i_keep, 1] = ctrl.rolling_avg.value * ctrl.THRESHOLD
+            keep[i_keep, 4] = decision
+            keep[i_keep, 5] = ctrl.mode == 'listening'
+            keep[i_keep, 6] = -int(ctrl.mode == 'playing')
+            i_keep = i_keep + 1
+            if i_keep == len(keep):
+                break
+    finally:
+        fig, (ax, bin_ax) = plt.subplots(2, 1, sharex=True)
+        # bin_ax = ax.twinx()
+        ax.plot(keep[:, :2])
+        bin_ax.plot(keep[:, 2:])
+        plt.show()
