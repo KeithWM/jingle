559ec76d448692fed38fde99466c0863c324a43b
KeithWM <keith@myerscough.nl>
2019-12-10 23:26:28 +0100

    Fixed missing $ in path

--- setup_path.sh
+++ setup_path.sh
@@ -1,4 +1,4 @@
 # Usage source FILENAME_OF_THIS_FILE
 LIB_DIR=$(cd lib; pwd)
 #See https://stackoverflow.com/questions/33615156/why-does-pythonpath-with-trailing-colon-add-current-directory-to-sys-path
-export PYTHONPATH="LIB_DIR{PYTHONPATH+":"}${PYTHONPATH-}"
+export PYTHONPATH="$LIB_DIR{PYTHONPATH+":"}${PYTHONPATH-}"
